7981f47213bbaad51e398cd9cf016db902dde8ac
tiberiuc <tibi@happysoft.ro>
2013-08-28 18:05:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get -y install ruby rubygems puppet curl zsh mc git git-flow nodejs chef graphicsmagick php5-cli php5-fpm php5-curl libimage-exiftool-perl optipng libjpeg-turbo-progs optipng ruby-dev libpcre3-dev cmake unzip nfs-common tig ruby-haml python-pip
+sudo apt-get -y install ruby rubygems puppet curl zsh mc git git-flow nodejs chef graphicsmagick php5-cli php5-fpm php5-curl libimage-exiftool-perl optipng libjpeg-turbo-progs optipng ruby-dev libpcre3-dev cmake unzip nfs-common tig ruby-haml python-pip libssl-dev
 
 sudo gem install --no-rdoc --no-ri haml sass coffee-script uglifier fssm listen aws-s3 json imagesize git-up rake
 
