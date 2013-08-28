6f3d8d4d20b9803855102620bddc08abdc02e3e6
tiberiuc <tibi@happysoft.ro>
2013-08-28 11:27:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install ruby rubygems puppet curl zsh mc rake git git-flow nodejs chef graphicsmagick php5-cli php5-fpm php5-curl libimage-exiftool-perl optipng libjpeg-turbo-progs optipng ruby-dev libpcre3-dev cmake unzip nfs-common tig ruby-haml python-pip
+sudo apt-get -y install ruby rubygems puppet curl zsh mc rake git git-flow nodejs chef graphicsmagick php5-cli php5-fpm php5-curl libimage-exiftool-perl optipng libjpeg-turbo-progs optipng ruby-dev libpcre3-dev cmake unzip nfs-common tig ruby-haml python-pip
 
 sudo gem install --no-rdoc --no-ri haml sass coffee-script uglifier fssm listen aws-s3 json imagesize gitup
 
@@ -10,6 +10,11 @@ sudo gem install --no-rdoc --no-ri haml sass coffee-script uglifier fssm listen 
 sudo add-apt-repository ppa:pi-rho/dev
 sudo apt-get update
 sudo apt-get install vim vim.nox tmux
+
+sudo apt-get autoremove
+sudo apt-get clean
+sudo gem cleanup
+rm -rf "$(gem env gemdir)"/doc/*
 
 pip install --user git+git://github.com/Lokaltog/powerline
 
